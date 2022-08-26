8900112f94fa9c33aaf98a2ef8da3819ee38c9b3
Bel Correa <isabelmrcorrea@gmail.com>
2022-08-25 21:48:04 -0300

    legal

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,15 @@
 let nome = "IsabelCorrea"
 let email = "isabelmrcorrea@gmail.com"
-let profissao = "donaDeCasa" 
-let idade = "36" 
-let lugarOndeDesejaConhecer = "TodoTerritorioNacional"
+let profissao = "donaDeCasa"
+let idade = "36"
+let viagemDosSonhos = "TodoTerritorioNacional"
+
+console.log("IsabelCorrea")
+console.log("isabelmrcorrea@gmail.com")
+console.log("donaDeCasa") 
+console.log("36") 
+console.log("TodoTerritorioNacional")
+
+let mensagem = "Ola, " + nome + "! Somos aqui da agencia de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aereas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade e " + idade + ", voce e " + profissao + " e o seu email e " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(mensagem)
