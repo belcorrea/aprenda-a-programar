58c04dcf7ba34e0814502d2a43346d735908f80e
Bel Correa <isabelmrcorrea@gmail.com>
2022-08-25 22:38:00 -0300

    pratica2

---
+++ tarefa-dia-2/pratica-2.js
@@ -0,0 +1,85 @@
+// Este é um comentário no código. Comentários não são cumpridos, eles servem pra gente deixar alguma mensagem
+// para outras pessoas que sabem o código. Agora vamos para o exercício.
+
+// Analise os 10 comandos 'if' seguir e tentar prever qual mensagem será exibida na tela.
+// Fique à vontade de rodar o código para verificar se suas respostas estão corretas.
+
+seja  a  =  10
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( a  ==  10 )  {
+    consola . log ( "é igual a 10" ) ;
+}  else  {
+    consola . log ( "não é igual a 10" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( a  >  10 )  {
+    consola . log ( "é maior que 10" ) ;
+}  else  {
+    consola . log ( "não é maior 10" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( 10  <=  a )  {
+    consola . log ( "é maior ou igual a 10" ) ;
+}  else  {
+    consola . log ( "não é maior ou igual a 10" ) ;
+}
+
+seja  b  =  verdadeiro
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( b  ==  verdadeiro )  {
+    consola . log ( "é igual a true" ) ;
+}  else  {
+    consola . log ( "não é igual a true" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( b )  {
+    consola . log ( "é considerado como verdade" ) ;
+}  else  {
+    consola . log ( "não é considerado como verdade" ) ;
+}
+
+seja  c  =  [ ]
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( c )  {
+    consola . log ( "é considerado como verdade" ) ;
+}  else  {
+    consola . log ( "não é considerado como verdade" ) ;
+}
+
+seja  d  =  [ 1 , 2 , 3 , 4 ]
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( d )  {
+    consola . log ( "é considerado como verdade" ) ;
+}  else  {
+    consola . log ( "não é considerado como verdade" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( d [ 0 ]  ==  1 )  {
+    consola . log ( "do elemento do índice 0 da variável 'd'é igual a 1" ) ;
+}  else  {
+    consola . log ( "do elemento do índice 0 da variável 'd'não é igual a 1" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( d [ 1 ]  ==  1 )  {
+    consola . log ( "do elemento do índice 1 da variável 'd'é igual a 1" ) ;
+}  else  {
+    consola . log ( "do elemento do índice 1 da variável 'd'não é igual a 1" ) ;
+}
+
+deixe  e  =  "Nome"
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if  ( typeof ( e )  ==  'string' )  {
+    consola . log ( "Verdade! O tipo de dado da variável 'e' é uma string" ) ;
+}  else  {
+    consola . log ( "Falso! O tipo de dado da variável 'e' não é uma string" ) ;
+}
