d4a167790a82d533d89eb1fb28647642b18f0a6b
Bel Correa <isabelmrcorrea@gmail.com>
2022-08-28 16:38:43 -0300

    pratica3

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,16 @@
+let n = 23
+
+if (n % 2 == 0) {
+    console.log (`${n} é par!`)
+
+} else {
+    console.log (`${n} é impar!`)
+}
+
+let i = 1
+
+while (i <= n) {
+    
+    console.log (i)
+    i = i + 2
+}
