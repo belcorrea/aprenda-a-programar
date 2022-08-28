8e1079128bbb14928bce412d8059dc05c5c200be
Bel Correa <isabelmrcorrea@gmail.com>
2022-08-28 16:08:42 -0300

    uhuuuu

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,9 @@
+let multiplicador = 9
+let i = 1
+
+while (i <= 10) {
+    let resultado = multiplicador * i
+    let textoDeExibicao = `${multiplicador} X ${i} = ${resultado}`
+    console.log (textoDeExibicao)
+    i = i + 1
+}
