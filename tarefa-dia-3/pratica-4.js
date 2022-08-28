7886cadca568de08b316cdc9606bd3aa3a24f235
Bel Correa <isabelmrcorrea@gmail.com>
2022-08-28 17:09:21 -0300

    tarefa opcional

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,23 @@
+17
+2
+7
+8
+5
+23
+15
+1500450271
+
+let n = 1500450271
+let i = 2
+let ehPrimo = true
+
+while (n % 2 != 0) 
+
+i = i + 1
+
+if (n == i) {
+    console.log (`Sim, o número ${n} é primo`)
+
+} else {
+    console.log(`Não, o número ${n} não é primo`)
+}
